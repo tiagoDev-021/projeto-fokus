d872ffee981a13f0d6d0cef4e297428d3e2a4a52
Tiago-dev <tiago.lopes.si2025@gmail.com>
2025-11-05 18:58:25 -0300

    adicionei comentário

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
 const html = document.querySelector('html');
 const focoBt =document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
@@ -128,3 +131,4 @@ function mostrarTempoDecorrido(){
 }
 
 mostrarTempoDecorrido()
+
